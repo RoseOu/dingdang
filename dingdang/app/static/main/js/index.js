7655a53da169e741cb0c2643371d430073612ecb
roseou <598959770@qq.com>
2017-12-01 15:34:24 +0800

    edit

--- dingdang/app/static/main/js/index.js
+++ dingdang/app/static/main/js/index.js
@@ -1,7 +1,7 @@
 var showName = document.getElementById('showName');
 var id = cookie.getCookie('id');
-id = 2; //chrome关闭跨域时使用
-fetch('http://120.24.4.254:5455/api/profile/' + id + '/').then(function(res){
+// id = 2; //chrome关闭跨域时使用
+fetch('http://120.24.4.254:5455/api/profile/' + id).then(function(res){
 	return res.json()
 }).then(value => {
     console.log(value)
