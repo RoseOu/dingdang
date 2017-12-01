0959f342ec34abcaf7a524ad12196394cbe9d321
roseou <598959770@qq.com>
2017-12-01 15:41:35 +0800

    edit

--- dingdang/app/static/main/js/index.js
+++ dingdang/app/static/main/js/index.js
@@ -1,9 +1,14 @@
 var showName = document.getElementById('showName');
 var id = cookie.getCookie('id');
+var logout = document.getElementById('logout');
 // id = 2; //chrome关闭跨域时使用
 fetch('http://120.24.4.254:5455/api/profile/' + id).then(function(res){
 	return res.json()
 }).then(value => {
     console.log(value)
     showName.innerHTML = value.email;
+})
+logout.addEventListener('click',function(){
+	cookie.clearCookie('id');
+	console.log(document.cookie)
 })
